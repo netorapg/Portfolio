cebdd3d4ebdaea97d3f736825cb3739c10adc83c
netorapg <renatoplatz11@outlook.com>
2024-07-08 21:49:09 -0300

    modificação realizada no footer e na barra de navegação, agora eles são uma coisa só. Trabalhando no tema escuro e claro da pagina

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-// Crie um elemento de botão
-const button = document.createElement('button');
-
-// Defina o texto do botão
-button.innerText = 'Navegar';
-
-// Adicione uma classe CSS ao botão para estilização
-button.classList.add('navigation-button');
-
-// Adicione um evento de clique ao botão
-button.addEventListener('click', () => {
-    // Lógica de navegação aqui
-});
-
-// Adicione o botão ao elemento pai desejado no DOM
-const parentElement = document.querySelector('.navigation-container');
-parentElement.appendChild(button);
+function alterarTema() {
+    const tema = document.body.getAttribute("data-tema");
+    const novoTema = tema =='dark' ? 'light' : 'dark';
+    document.body.setAttribute("data-tema", novoTema);
+    localStorage.setItem('tema', novoTema);
+}
